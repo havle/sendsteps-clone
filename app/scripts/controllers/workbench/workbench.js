d6c0d3f4ec9578f7a98818d7c32634a9b0ee47b1
Ha Le <contact@ha-le.com>
2013-10-12 11:33:03 +1100

    Workbench - poll creation

--- app/scripts/controllers/workbench/workbench.js
+++ app/scripts/controllers/workbench/workbench.js
@@ -2,22 +2,34 @@
 
 angular.module('sendstepsCloneApp')
 	.controller('WorkBenchCtrl', function($scope) {
-		$scope.newItem = "";
-		$scope.items = ['1','2','3'];
-		$scope.pollTitle = "";
-
+		$scope.polls = [];
 		$scope.addItem = function(){
 			if(angular.isUndefined($scope.newItem)
 				|| $scope.newItem == "")
 				return;
 			$scope.items.push($scope.newItem);
 			$scope.newItem = "";
-		}
+		};
 
-		$scope.savePoll = new function(){
+		$scope.savePoll = function(){
 			if($scope.items.count <= 0)
 				return;
 
+			if($scope.pollTitle == "")
+			{
+				// TODO: change this to nice flash up top
+				alert("Must include a poll title");
+			}
 
+			$scope.polls.push({"title": $scope.pollTitle, "items": $scope.items});
+			$scope.resetVars();
+		}
+
+		$scope.resetVars = function(){
+			$scope.items = [
+				'Thor',
+				'Gandi',
+				'Superman'];
+			$scope.pollTitle = "Who's your favourite Marvel hero";
 		}
 	});
